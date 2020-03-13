3289e8b35fe02980b6926c6e36b2002d1b4dfb95
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-03-13 10:41:30 +0100

    - work in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import Posts from './components/Posts'
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,8 +23,9 @@ function App() {
     console.log(posts);
     console.log(loading);
     return (
-        <div className="App">
-            <h1>Future header</h1>
+        <div className="container mt-5">
+            <h1 className="text-primary mb-3">My blog</h1>
+            <Posts posts={posts} loading={loading}/>
         </div>
     );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,8 +5,10 @@ const Posts = ({post, loading}) => {
         return <h2>Loading...</h2>
     }
     return <ul className="list-group mb-4">
-        {posts.map(post => (
-            <li key={post.id} className="list-group-item"/>
+        {post.map(post => (
+            <li key={post.id} className="list-group-item">
+                {post.title}
+            </li>
         ))}
     </ul>
 };
