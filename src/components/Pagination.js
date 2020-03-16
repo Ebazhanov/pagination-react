691c3c86f577808c2ff8ed0b2ccee3ca28561dd9
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-03-16 19:40:41 +0100

    - upd

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,7 +12,7 @@ const Pagination = ({postsPerPage, totalPosts, paginate}) => {
             <ul className='pagination'>
                 {pageNumbers.map(number => (
                     <li key={number} className='page-item'>
-                        <a onClick={() => paginate(number)} href='!#' className='page-link'>
+                        <a onClick={() => paginate(number)} href='' className='page-link'>
                             {number}
                         </a>
                     </li>
